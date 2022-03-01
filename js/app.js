55901d7c517b7458ab75991f1e571d422f65051a
MdMehedi-Hasan <mh19115@gmail.com>
2022-03-02 02:03:53 +0600

    details added

--- js/app.js
+++ js/app.js
@@ -30,6 +30,27 @@ const display = (products) =>{
     }
 }
 const details = infoId =>{
-const detailsUrl =`https://openapi.programming-hero.com/api/phone/${infoId}`
-console.log(detailsUrl);
+    const detailsUrl =`https://openapi.programming-hero.com/api/phone/${infoId}`
+    fetch(detailsUrl)
+        .then(res => res.json())
+        .then(data => console.log(data))
+    const parent2 = document.getElementById('parent2')
+    parent2.textContent='';
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.classList.add('g-0')
+    div.innerHTML=`
+    <div  class="row g-0">
+    <div class="col-md-4">
+    <img src="..." class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+        </div>
+    </div>`
+parent2.appendChild(div)
 }
