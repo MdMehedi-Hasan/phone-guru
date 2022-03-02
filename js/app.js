0130902ae610f9c0d94607c7abdb26d5415f7106
MdMehedi-Hasan <mh19115@gmail.com>
2022-03-02 19:15:20 +0600

    Main project finished

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Getting Search input value
 document.getElementById('button-addon2').addEventListener('click',() =>{
     const inputField = document.getElementById('input');
     const inputValue =inputField.value;
@@ -7,16 +8,14 @@ document.getElementById('button-addon2').addEventListener('click',() =>{
     .then(data => display(data));
     inputField.value='';
 })
+// Getting search result/Product
 const display = (products) =>{
-    // console.log(products)
     const parent2 = document.getElementById('parent2')
     parent2.textContent='';
     const parent = document.getElementById('result-section');
     parent.textContent='';
-    const data = products.data;
-    // console.log(products.data.length)
+    const data = products.data.slice(0,20);
     for(const product of data){
-        // console.log(product)
         const div = document.createElement('div');
         div.classList.add('col');
         div.classList.add('rounded');
@@ -31,8 +30,8 @@ const display = (products) =>{
     `
     parent.appendChild(div);
     }
-    const length = products.data.length
-    if(length == 0){
+    // Validating null search/Wrong search
+    if(products.data.length == 0){
         const validation = document.getElementById('validation');
         validation.classList.add('d-block')
     }
@@ -40,17 +39,16 @@ const display = (products) =>{
         const validation = document.getElementById('validation');
         validation.classList.add('d-none')
     }
-    console.log(products.data.length)
 }
-
+// Displaying indivisual product details
 const details = infoId =>{
     const detailsUrl =`https://openapi.programming-hero.com/api/phone/${infoId}`
     fetch(detailsUrl)
         .then(res => res.json())
         .then(data => detailsCard(data))
 }
+// supporting above function to execute
 const detailsCard = sec => {
-    console.log(sec)
     const parent2 = document.getElementById('parent2')
     parent2.textContent='';
     const div = document.createElement('div')
@@ -65,7 +63,14 @@ const detailsCard = sec => {
         <div class="card-body">
             <h5 class="card-title">${sec.data.brand} ${sec.data.name}</h5>
             <h6 class="card-text">Main Features:-</h6>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <ul style="list-style-type: none;">
+            <li><strong>Chipset:</strong> ${sec.data.mainFeatures.chipSet}</li>
+            <li><strong>Display-Size:</strong> ${sec.data.mainFeatures.displaySize}</li>
+            <li><strong>Memory:</strong> ${sec.data.mainFeatures.memory}</li>
+            <li><strong>Storage:</strong> ${sec.data.mainFeatures.storage}</li>
+            <li><strong>Sensors:</strong> ${sec.data.mainFeatures.sensors}</li>
+            </ul>
+            <p><strong>Others:-</strong>(Bluetooth: ${sec.data.others.Bluetooth}, GPS: ${sec.data.others.GPS}, NFC: ${sec.data.others.NFC}, Radio: ${sec.data.others.Radio}, USB: ${sec.data.others.USB}, WLAN: ${sec.data.others.WLAN})</p>
             <p class="card-text"><small class="text-muted">${sec.data.releaseDate}</small></p>
         </div>
         </div>
